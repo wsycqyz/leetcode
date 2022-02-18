6ded960886f5dc6d8d54331517769dce23fb2793
Shiyan <wsycqyz@gmail.com>
2022-02-18 13:37:33 +0800

    204: 埃拉托斯特尼筛法

--- 204.count-primes.cpp
+++ 204.count-primes.cpp
@@ -4,6 +4,9 @@
  * [204] Count Primes
  */
 
+埃拉托斯特尼筛法
+https://zh.wikipedia.org/wiki/%E5%9F%83%E6%8B%89%E6%89%98%E6%96%AF%E7%89%B9%E5%B0%BC%E7%AD%9B%E6%B3%95
+
 // @lc code=start
 class Solution {
 public:
