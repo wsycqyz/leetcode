59ac780e2d65f0c286bfc47999c3205490afa65b
Shiyan <wsycqyz@gmail.com>
2022-03-15 18:01:19 +0800

    Modify 7

--- 7.reverse-integer.cpp
+++ 7.reverse-integer.cpp
@@ -13,7 +13,12 @@ public:
         while(x) {
             a=x%10;
             x/=10;
-            if ((tmp>0 && tmp>(INT_MAX)/10) || (tmp<0 && tmp<(INT_MAX)/10)) {
+            if ((tmp>0 && tmp>INT_MAX/10) 
+             || (tmp<0 && tmp<INT_MIN/10)) {
+                return 0;
+            }
+            if ((tmp>0 && tmp==INT_MAX/10 && a>INT_MAX%10)
+            ||  (tmp<0 && tmp==INT_MIN/10 && a<INT_MIN%10)) {
                 return 0;
             }
             tmp=tmp*10+a;
