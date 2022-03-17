361859e41b664cff60f7e15adecb9b811995abce
Shiyan <wsycqyz@gmail.com>
2022-03-17 11:46:00 +0800

    Modify 445 add two numbers ii

--- 445.add-two-numbers-ii.cpp
+++ 445.add-two-numbers-ii.cpp
@@ -17,60 +17,37 @@
  */
 class Solution {
 public:
+    ListNode* reverseList(ListNode *head) {
+        if (head==NULL || head->next==NULL) {
+            return head;
+        }
+        ListNode *p = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return p;
+    }
+
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int m=0,n=0,x=0;
-        ListNode *a=l1,*b=l2,*re=NULL,*tmp=NULL;
-        while(a!=NULL) {a=a->next; m++;}
-        while(b!=NULL) {b=b->next; n++;}
-        if (m>n) {
-            a=l1;
-            b=l2;
-        } else {
-            a=l2;
-            b=l1;
-            swap(m,n);
+        int val,carry, val1, val2;
+        ListNode dummy;
+        ListNode *cur=&dummy, *tmp;
+        carry = 0;
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
+        while(l1 || l2 || carry) {
+            if (l1!=NULL) val1 = l1->val; else val1=0;
+            if (l2!=NULL) val2 = l2->val; else val2=0;
+            val = val1+val2+carry;
+            carry = val/10;
+            val=val%10;
+            tmp = new ListNode(val);
+            cur->next = tmp;
+            cur = tmp;
+            if (l1!=NULL) l1=l1->next;
+            if (l2!=NULL) l2=l2->next;
         }
-        //cout << m << " " << n << endl;
-        while(m>0&&n>0) {
-            if (m>n) {
-                tmp=new ListNode(a->val);
-                tmp->next=re;
-                //cout << " " << tmp->val;
-                re=tmp;
-                a=a->next;
-                m--;
-            } else {
-                tmp=new ListNode(a->val+b->val);
-                tmp->next=re;
-                //cout << " " << tmp->val;
-                re=tmp;
-                a=a->next;
-                b=b->next;
-                m--;
-                n--;
-            }
-        }
-        a=re;
-        //while(a!=NULL) { cout << a->val << " "; a=a->next;}
-        cout << endl;
-        a=NULL;
-        while(re!=NULL) {
-            re->val+=x;
-            x=(re->val)/10;
-            re->val=(re->val)%10;
-            b=re->next;
-            re->next=a;
-            a=re;
-            re=b;
-        }
-
-        if (x) {
-            tmp=new ListNode(1);
-            tmp->next=a;
-            a=tmp;
-        }
-
-        return a;
+        cur = reverseList(dummy.next);
+        return cur;
     }
 };
 // @lc code=end
