94cbcada99ccd1c25c45adcc55214ee673428e4c
Shiyan <wsycqyz@gmail.com>
2022-03-17 11:30:42 +0800

    Modify 2

--- 2.add-two-numbers.cpp
+++ 2.add-two-numbers.cpp
@@ -18,38 +18,23 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int x=0,y=0;
+        int val,carry, val1, val2;
         ListNode dummy;
-        ListNode *cur=&dummy;
-        ListNode *tmp, *l3;
-        while(l1&&l2) {
-            x = (l1->val + l2->val + y) % 10;
-            y = (l1->val + l2->val + y) / 10;
-            tmp = new ListNode(x);
+        ListNode *cur=&dummy, *tmp;
+        carry = 0;
+        while(l1 || l2 || carry) {
+            if (l1!=NULL) val1 = l1->val; else val1=0;
+            if (l2!=NULL) val2 = l2->val; else val2=0;
+            val = val1+val2+carry;
+            carry = val/10;
+            val=val%10;
+            tmp = new ListNode(val);
             cur->next = tmp;
-            tmp->next = NULL;
             cur = tmp;
-            l1 = l1->next; l2 = l2->next;
+            if (l1!=NULL) l1=l1->next;
+            if (l2!=NULL) l2=l2->next;
         }
-        if (l1) {
-            l3 = l1;
-        } else {
-            l3 = l2;
-        }
-        while(l3) {
-            x = (l3->val + y) % 10;
-            y = (l3->val + y) / 10;
-            tmp = new ListNode(x);
-            cur->next = tmp;
-            tmp->next = NULL;
-            cur = tmp;
-            l3 = l3->next;
-        }
-        if (y) {
-            tmp = new ListNode(y);
-            cur->next = tmp;
-            tmp->next = NULL;
-        }
+        
         return dummy.next;
     }
 };
