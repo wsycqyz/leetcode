127a749a0489a6f909a39169c4977257dc8ae49b
Shiyan <wsycqyz@gmail.com>
2022-03-25 11:37:51 +0800

    Modify 258 wiki page: Digital root

--- 258.add-digits.cpp
+++ 258.add-digits.cpp
@@ -4,6 +4,11 @@
  * [258] Add Digits
  */
 
+https://en.wikipedia.org/wiki/Digital_root
+b based,
+n!= 0: result = 1 + (n-1) % (b-1)
+n = 0: result = 0
+
 // @lc code=start
 class Solution {
 public:
